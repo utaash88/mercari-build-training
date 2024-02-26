971b34c6ff1e42cc063f82bcb4bb5c6bc432f312
utaash88 <petit.pomme14@gmail.com>
2024-02-27 08:21:43 +0900

    step4-1　エラー修正

--- python/main.py
+++ python/main.py
@@ -12,18 +12,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-""" conn=sqlite3.connect(dbname)
-cur=conn.cursor() """
-
-
-""" cur.execute(CREATE TABLE items (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    category TEXT,
-    image_name TEXT
-)) """
-
-
 logger = logging.getLogger("uvicorn")
 logger.level = logging.INFO
 
@@ -37,38 +25,12 @@ app.add_middleware(
 )
 
 
-db_path = pathlib.Path(__file__).parent/"db"/"mercari.sqlite3"
-images = pathlib.Path(__file__).parent/"python"/ "images"
+db_path = pathlib.Path(__file__).parent.resolve() /"mercari.sqlite3"
+images = pathlib.Path(__file__).parent.resolve() /"images"
 items_json_path = pathlib.Path(__file__).parent.resolve() / "items.json"
-
-#create database
 
 conn = sqlite3.connect(db_path)
 cur = conn.cursor()
-
-def create_table():
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS categories (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE
-        )
-    """)
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS items (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            category_id INTEGER NOT NULL,
-            image_name TEXT NOT NULL,
-            FOREIGN KEY (category_id) REFERENCES categories(id)
-        )
-    """)
-    conn.commit()
-    
-create_table()
-
-#def save_image(file,filename):
-    #with open(images / filename, "wb") as image:
-        #image.write(file)
 
 def save_item_db(name, category, image_name):
     cur.execute("INSERT INTO items (name, category, image_name) VALUES (?, ?, ?)", (name, category, image_name))
@@ -77,20 +39,6 @@ def save_item_db(name, category, image_name):
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
-
-""" items_list=[]
-@app.post("/items")
-def add_item(name: str = Form(...), category:str=Form(...), image:UpladFile = File(...)):
-    logger.info(f"Receive item: {name}, category: {category}, image: {image}")
-    
-    file_content = image.file.read()
-    hash_value = hashlib.sha256(file_content).hexdigest()
-    image_filename = f"{hash_value}.jpg"
-    save_image(file_content, image_filename)
-
-    save_item_db(name, category, image_filename)
-    return {"message": f"item received: {name},category:{category}","image_name": image_filename}
- """
 
  
 
@@ -109,7 +57,7 @@ async def add_item(name: str = Form(...), category: str = Form(...), image: Uplo
         f.write(contents)
 
     #sqliteに追加
-    cur.execute("SELECT id FROM categories WHERE name = ?", (category,))
+    """ cur.execute("SELECT id FROM categories WHERE name = ?", (category,))
     category_result = cur.fetchone()
 
     if not category_result:
@@ -120,8 +68,9 @@ async def add_item(name: str = Form(...), category: str = Form(...), image: Uplo
         category_id = category_result[0]
 
     cur.execute("INSERT INTO items (name, category_id, image_name) VALUES (?, ?, ?)", (name, category_name, image_filename))
-    conn.commit()
+    conn.commit() """
 
+    save_item_db(name, category, image_filename)
 
     # 新しいアイテムをJSONファイルに追加
     new_item = {"name": name, "category": category, "image_name": image_filename}
@@ -132,16 +81,6 @@ async def add_item(name: str = Form(...), category: str = Form(...), image: Uplo
         json.dump(items, f, indent=4)
 
     return {"message": f"item received: {name}, {category}, {image_filename}"}
-
-
-
-
-""" @app.get("/items")
-def get_items():
-    cur.execute("SELECT name, category, image_name FROM items")
-    items=cur.fetchall()
-    cur.execute("SELECT category_id FROM items INNER JOIN category ON items.category_id=category.id")
-    return {"items":items} """
 
 
 @app.get("/items")
@@ -164,20 +103,12 @@ async def get_image(image_name):
 
     return FileResponse(image)
 
-"""@app.get("/items/{item_id}")
-def get_item(item_id: int= Path(..., title="The ID of the item to get")):
-    items_data=load_item()
-     existing_items = items_data.get("items", [])
 
-    if item_id < len(existing_items):
-        item = existing_items[item_id-1]
-        return item """
-
-""" @app.get("/search/{search_item}")
+@app.get("/search/{search_item}")
 def search_item(search_item:str):
     cur.execute("SELECT name, category, image_name FROM items WHERE name LIKE ?",("%"+search_item+"%",))
     items= cur.fetchall()
     return {"items":items}
 
-cur.close()
-conn.close() """
+conn.commit()
+conn.close()
